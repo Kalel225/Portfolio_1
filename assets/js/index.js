eddb9b25ba095d7d5e88339c431eab64fbcec28f
KalEl <126879696+Kalel225@users.noreply.github.com>
2026-01-13 12:27:10 +0000

    ajouts des animations et des medias queries

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,24 @@
 window.addEventListener("load",()=>{
     document.getElementById("loader").style.display="none"
 ;});
+const hamburger = document.getElementById("hamburger");
+const navMobile = document.getElementById("navMobile");
+
+hamburger.addEventListener("click",()=>{
+    hamburger.classList.toggle("active");
+    navMobile.classList.toggle("open");
+});
+document.getElementById("scrollProject").onclick=()=>{
+    document.getElementById("projects").scrollIntoView({
+        behavior:"smooth"
+    });
+}
+const observer = new IntersectionObserver(entries=>{
+    entries.forEach(entry=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+},{threshold:0.2});
+
+document.querySelectorAll(".animate").forEach(el=>observer.observe(el));
